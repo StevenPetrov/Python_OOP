8bfc8eb3ec652ebd5d95378e43c73a9228814312
Steven Petrov <91495882+StevenPetrov@users.noreply.github.com>
2022-07-17 15:04:17 +0300

    tasks

---
+++ 03. Classes and Objects/4. Glass.py
@@ -0,0 +1,27 @@
+class Glass:
+    content = 0
+    capacity = 250
+
+    def __init__(self):
+        pass
+
+    def fill(self, ml):
+        if self.capacity - self.content < ml:
+            return f"Cannot add {ml} ml"
+        self.content += ml
+        return f"Glass filled with {ml} ml"
+
+    def empty(self):
+        self.content = 0
+        return f"Glass is now empty"
+
+    def info(self):
+        return f"{self.capacity - self.content} ml left"
+
+
+glass = Glass()
+print(glass.fill(100))
+print(glass.fill(200))
+print(glass.empty())
+print(glass.fill(200))
+print(glass.info())
